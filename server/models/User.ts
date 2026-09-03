38b2ef7c3b9f717b57d3e4f757e1a6a3d4909ab2
Manas Rout <mrout0000007@gmail.com>
2026-09-03 19:13:33 +0530

    Created API for Authentication

---
+++ server/models/User.ts
@@ -0,0 +1,32 @@
+import {model, Schema} from 'mongoose'
+
+export interface Iuser extends Document{
+  name: string; 
+  email: string; 
+  password?: string; 
+  phone?: string; 
+  role: "user" | "admin" | "owner";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new Schema(
+  {
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true,trim: true, lowercase: true },
+  password: { type: String, required: false, minlength: 6 },
+  phone: { type: String, trim: true, minlength: 6},
+  role: { type: String, enum: ["user", "admin", "owner"], default: "user" },
+  },
+ {timestamps: true}
+
+)
+// Remove pass when convert to Json 
+UserSchema.set("toJSON", {
+  transform: (doc , ret) => {
+    delete ret.password;
+    return ret;
+  },
+});
+
+export const User = model<Iuser>('User', UserSchema);
--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
 
 const app = express();
 
@@ -18,6 +20,17 @@ const port = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/auth', authRoutes);
+
+//Global error Handler 
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+    console.error(err.stack);
+    res.status(500).json({ 
+      message: err.message || "Internal Server Error" , 
+      stack : process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
