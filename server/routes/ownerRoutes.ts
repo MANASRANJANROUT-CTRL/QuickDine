2f60d670768773de70b5dd405a642bc2523821da
Manas Rout <mrout0000007@gmail.com>
2026-09-04 18:17:43 +0530

    created API for restaurant owner

---
+++ server/routes/ownerRoutes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { createOwnerRestaurant, getOwnerBookings, getOwnerRestaurant, updateBookingStatus, updateOwnerRestaurant } from "../controllers/ownerController.js";
+import upload from "../config/multer.js";
+import { ownerOnly, protect } from "../middlewares/auth.js";
+
+
+const ownerRouter = Router();
+
+ownerRouter.use(protect)
+ownerRouter.use(ownerOnly)
+
+
+ownerRouter.get("/restaurant", getOwnerRestaurant)
+ownerRouter.post("/restaurant",upload.single("image"), createOwnerRestaurant)
+ownerRouter.post("/restaurant",upload.single("image"), updateOwnerRestaurant)
+ownerRouter.post("/bookings", getOwnerBookings)
+ownerRouter.post("/bookings/:id/status", updateBookingStatus)
+
+export default ownerRouter;
